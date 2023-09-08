a6a2311b7d6276906a9822e6b93156942981f380
Koigor97 <turaysamuel101@yahoo.com>
2023-09-08 17:14:32 +0200

    Update components in App.js

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -25,15 +27,31 @@ function Header() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Take what you need for your tavels 😉</h3>
 
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -41,7 +59,12 @@ function Form() {
         ))}
       </select>
 
-      <input type="text" placeholder="item..." />
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
