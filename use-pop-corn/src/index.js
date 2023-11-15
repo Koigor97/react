048dd3553b3bf455c2b22f7f6cdcd4c4d62903bb
Koigor97 <turaysamuel101@yahoo.com>
2023-11-15 15:52:27 +0200

    modify project index.js file

--- use-pop-corn/src/index.js
+++ use-pop-corn/src/index.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} color={"#fcc419"} size={30} />
+    <StarRating maxRating={10} color={"orangeRed"} defaultRating={3} />
+    <StarRating
+      maxRating={3}
+      color={"lime"}
+      size={35}
+      satisfaction={["Nice", "Great", "Amazing"]}
+    />
+    <Testing />
   </React.StrictMode>
 );
+
+function Testing() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <>
+      <StarRating
+        color={"Pink"}
+        maxRating={10}
+        size={30}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </>
+  );
+}
