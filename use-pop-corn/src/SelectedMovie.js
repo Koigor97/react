75f9ab18611f934ebacf7206a802f133e3c7b3f9
Koigor97 <turaysamuel101@yahoo.com>
2023-11-25 17:01:17 +0200

    modify selectedMovie component

--- use-pop-corn/src/SelectedMovie.js
+++ use-pop-corn/src/SelectedMovie.js
@@ -74,6 +74,23 @@ export function SelectedMovie({
     [title]
   );
 
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
