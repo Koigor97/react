f84422d0fbc4ecab751e0a1becc4bb86f24fc458
Koigor97 <turaysamuel101@yahoo.com>
2023-12-27 22:59:37 +0200

    create dummyData JS file

---
+++ priority-handler/src/data/dummyTaskData.js
@@ -0,0 +1,22 @@
+const DummyTasks = [
+  {
+    id: 1,
+    completed: false,
+    priority: "high",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 2,
+    completed: false,
+    priority: "low",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+  {
+    id: 3,
+    completed: false,
+    priority: "medium",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
+  },
+];
+
+export default DummyTasks;
