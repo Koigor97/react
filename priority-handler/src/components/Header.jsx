851c1b5a16268c4b4727e2093afee04f5d823230
Koigor97 <turaysamuel101@yahoo.com>
2023-12-27 22:55:11 +0200

    modify header component

---
+++ priority-handler/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <div>
+        <h1>Priority Handler 📋</h1>
+        <p>Tackle Priorities - Stay on Top </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
