7d86a91dfd89c1fcbd29672dfb448cd517ed6f89
Koigor97 <turaysamuel101@yahoo.com>
2023-12-27 12:19:41 +0200

    add priority handler project

---
+++ priority-handler/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import ListItem from "./components/ListItem";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main>
+        <ListItem />
+      </Main>
+    </>
+  );
+}
+
+export default App;
