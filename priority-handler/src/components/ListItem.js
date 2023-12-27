bd0843ca60a74ca527fcaa55beddedaa324e0154
Koigor97 <turaysamuel101@yahoo.com>
2023-12-27 12:23:55 +0200

    add Header, ListItem, Main.js components

---
+++ priority-handler/src/components/ListItem.js
@@ -0,0 +1,10 @@
+function ListItem() {
+  return (
+    <div className="list-item-div">
+      <span className="priority-box">high</span>
+      <div>The first Priority Item</div>
+    </div>
+  );
+}
+
+export default ListItem;
