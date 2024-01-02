862db71cf6a5dc1cf8881c618e30958ea13d70b5
Koigor97 <turaysamuel101@yahoo.com>
2024-01-03 00:01:41 +0200

    remove unnecessary console.logs

--- priority-handler/src/components/ListItem.jsx
+++ priority-handler/src/components/ListItem.jsx
@@ -2,38 +2,23 @@ import { FaTimes } from "react-icons/fa";
 import Card from "./shared/Card";
 
 function ListItem({ task, setTask, dummyTask }) {
-  console.log(dummyTask);
   function handleDelete(id) {
     if (window.confirm("Are you sure you want to delete this task?")) {
       setTask(dummyTask.filter((task) => task.id !== id));
     }
   }
 
-  function handleCheckBox(id) {
-    setTask(
-      dummyTask.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
-  }
-
   return (
-    <Card darkMode={false} dummyTask={dummyTask.completed}>
+    <Card darkMode={false}>
       <span className={`priority-box ${task.priority}`}>{task.priority}</span>
-      <button className="close-btn" onChange={() => handleDelete(task.id)}>
+      <button className="close-btn" onClick={() => handleDelete(task.id)}>
         <FaTimes color="purple" />
       </button>
       <div>{task.text}</div>
       <form>
         <label className="checkbox-label">
           complete
-          <input
-            type="checkbox"
-            name="done"
-            id="done"
-            value={task.completed}
-            onChange={() => handleCheckBox(task.id)}
-          />
+          <input type="checkbox" name="done" id="done" value={1} />
         </label>
       </form>
     </Card>
