340f392b052f9294144c8e9544821671e09159ac
Koigor97 <turaysamuel101@yahoo.com>
2024-01-08 16:54:27 +0200

    modify aboutPage component

--- priority-handler/src/components/pages/AboutPage.jsx
+++ priority-handler/src/components/pages/AboutPage.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
 import Card from "../shared/Card";
 
 function AboutPage() {
   return (
-    <Card styles={{ marginTop: "1rem" }}>
-      <h1>About Page</h1>
-      <p>This is the about page</p>
-    </Card>
+    <div className="about-card">
+      <Card>
+        <h2>About The Priority Handler Project</h2>
+        <p>
+          This is a react app to handle your priorities. Self explanatory right
+          🤓 <br />
+          Version 1.0.0
+        </p>
+        <p>
+          <Link to="/">Go Back</Link>
+        </p>
+      </Card>
+    </div>
   );
 }
 
