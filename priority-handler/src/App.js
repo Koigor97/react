1b595d64b15ec15ae4c05c09d466b8de504b771c
Koigor97 <turaysamuel101@yahoo.com>
2024-01-08 15:09:14 +0200

    import aboutpage component and react-router-dom routes

--- priority-handler/src/App.js
+++ priority-handler/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
 import ListItem from "./components/ListItem";
 import TaskStats from "./components/TaskStats";
 import TaskForm from "./components/TaskForm";
+import AboutPage from "./components/pages/AboutPage";
 
 import { motion, AnimatePresence } from "framer-motion";
 
@@ -19,28 +21,41 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Main>
-        <TaskForm handleNewTask={handleAddNewTask} />
-        <TaskStats dummyTask={dummyTask} />
-        <AnimatePresence>
-          {dummyTask.map((tasks) => (
-            <motion.div
-              key={tasks.id}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <ListItem
-                key={tasks.id}
-                task={tasks}
-                dummyTask={dummyTask}
-                setTask={setDummyTask}
-              />
-            </motion.div>
-          ))}
-        </AnimatePresence>
-      </Main>
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Main>
+                  <TaskForm handleNewTask={handleAddNewTask} />
+                  <TaskStats dummyTask={dummyTask} />
+                  <AnimatePresence>
+                    {dummyTask.map((tasks) => (
+                      <motion.div
+                        key={tasks.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                      >
+                        <ListItem
+                          key={tasks.id}
+                          task={tasks}
+                          dummyTask={dummyTask}
+                          setTask={setDummyTask}
+                        />
+                      </motion.div>
+                    ))}
+                  </AnimatePresence>
+                </Main>
+              </>
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
