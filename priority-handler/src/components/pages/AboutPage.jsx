a7638280eb24c689fab15cb75850a44c6f406299
Koigor97 <turaysamuel101@yahoo.com>
2024-01-08 15:09:45 +0200

    create about page component

---
+++ priority-handler/src/components/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import Card from "../shared/Card";
+
+function AboutPage() {
+  return (
+    <Card styles={{ marginTop: "1rem" }}>
+      <h1>About Page</h1>
+      <p>This is the about page</p>
+    </Card>
+  );
+}
+
+export default AboutPage;
