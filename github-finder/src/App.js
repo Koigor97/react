2d28e11e952091f1005ad4fd21b9d078a1d939cf
Koigor97 <turaysamuel101@yahoo.com>
2024-02-08 16:06:16 +0200

    import Navbar and Footer components

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main className="container mx-auto px-3 pb-12">Content</main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
