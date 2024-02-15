f216d1509dd8d53f9503bb287fc93e67332f57eb
Koigor97 <turaysamuel101@yahoo.com>
2024-02-15 22:49:36 +0200

    complied typescript to javascript file

---
+++ ts-refresher/test-app.js
@@ -0,0 +1,7 @@
+var sam = {
+    name: "Sam Brewah",
+    age: 26,
+    job: "Software Developer",
+    degree: ["Bachelor in History and Politics", "Pathway Connect Certificate"],
+};
+console.log(sam);
