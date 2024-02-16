708c571dd86434f4745cc064629b76987dc10aab
Koigor97 <turaysamuel101@yahoo.com>
2024-02-16 15:58:24 +0200

    modify app.tsx file

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,12 +1,36 @@
-import CourseGoal from "./components/CourseGoals.tsx";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import Header from "./components/Header.tsx";
+import ListItem from "./components/ListItem.tsx";
+
+import imgLogo from "./assets/goals.jpg";
+import { GoalsType } from "./types/index.ts";
 
 export default function App() {
+  const [goals, setGoals] = useState<GoalsType[]>([]);
+
+  function handleAddGoal() {
+    console.log("Click");
+    setGoals((previousGoals) => {
+      const newGoal: GoalsType = {
+        title: "Finish React",
+        description: "I want to finish learning React by the End of Nextweek",
+        id: uuidv4(),
+      };
+      return [...previousGoals, newGoal];
+    });
+  }
+
   return (
     <main>
-      <CourseGoal
-        title="Learning React with TypeScript"
-        description="Learn how build your React projects with TypeScript"
-      />
+      <Header image={{ src: imgLogo, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
+
+      <button onClick={handleAddGoal}>Add Goal</button>
+
+      <ListItem goals={goals} />
     </main>
   );
 }
