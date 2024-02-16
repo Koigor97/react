73ade053463c8c7c5330c56ce074052ec45052e9
Koigor97 <turaysamuel101@yahoo.com>
2024-02-16 12:00:22 +0200

    import courseGoal component

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import CourseGoal from "./components/CourseGoals.tsx";
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal
+        title="Learning React with TypeScript"
+        description="Learn how build your React projects with TypeScript"
+      />
+    </main>
+  );
+}
