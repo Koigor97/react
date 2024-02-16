9da100237f04dd49614e7aa45be8c98a51bfa0b4
Koigor97 <turaysamuel101@yahoo.com>
2024-02-16 14:17:30 +0200

    refactor CourseGoals component

--- react-typescript/src/components/CourseGoals.tsx
+++ react-typescript/src/components/CourseGoals.tsx
@@ -1,15 +1,13 @@
-export default function CourseGoal({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) {
+import { PropsWithChildren } from "react";
+
+type CourseGoalsType = PropsWithChildren<{ title: string }>;
+
+export default function CourseGoal({ title, children }: CourseGoalsType) {
   return (
     <article>
       <div className="">
         <h2>{title}</h2>
-        <p>{description}</p>
+        {children}
       </div>
       <button>Delete</button>
     </article>
