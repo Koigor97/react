3094bba1f26b87ee81dbabb006ef9861346d6f06
Koigor97 <turaysamuel101@yahoo.com>
2024-02-20 07:56:54 +0200

    import and implement InfoBox

--- react-typescript/src/components/ListItem.tsx
+++ react-typescript/src/components/ListItem.tsx
@@ -1,16 +1,40 @@
 import CourseGoal from "./CourseGoals";
+import InfoBox from "./InfoBox";
+
 import { GoalsListType } from "../types";
+import { ReactNode } from "react";
 
 export default function ListItem({ goals, onDelete }: GoalsListType) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode="hint">
+        You have no course goals yet. Start adding some.
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning" severity="high">
+        You're collecting a lot of goals. Slow down
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
