3136ad3f217b0e7588ed2a79a3dd680abad4b075
Koigor97 <turaysamuel101@yahoo.com>
2024-03-18 20:43:42 +0200

    import account and customer reducers

--- redux-proj/src/index.js
+++ redux-proj/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import "./store";
+import store from "./store";
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "customer/createCustomer",
+  payload: { fullName: "Samuel Turay", ntlID: "266#badd7i" },
+});
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
