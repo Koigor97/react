bae85538da11a0d6948479ac18a904c46839b754
Koigor97 <turaysamuel101@yahoo.com>
2024-03-19 10:10:10 +0200

    wrap app in provider component and pass store as prop

--- redux-proj/src/index.js
+++ redux-proj/src/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
 import store from "./store";
 
-console.log(store.getState());
-
-store.dispatch({
-  type: "customer/createCustomer",
-  payload: { fullName: "Samuel Turay", ntlID: "266#badd7i" },
-});
-console.log(store.getState());
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
