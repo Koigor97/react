100c30fe72e4b0edd167e2cdacaa4bde8c27c8e2
Koigor97 <turaysamuel101@yahoo.com>
2024-03-20 19:35:49 +0200

    modify accountOperations  file

--- redux-proj/src/features/accounts/AccountOperations.js
+++ redux-proj/src/features/accounts/AccountOperations.js
@@ -19,9 +19,9 @@ function AccountOperations() {
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount, currency));
+    dispatch(deposit(depositAmount));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
